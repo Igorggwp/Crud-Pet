75a964dcf18a6676e1e8b2387f812d0a156ea886
Igorggwp <igorggwpp@gmail.com>
2023-09-22 11:17:21 -0300

    Adcionei nova funcionalidade em excluir.h

--- src/excluir.h
+++ src/excluir.h
@@ -1,8 +1,7 @@
-#include <stdio.h>
-
 int excluir(int *tamanho) {
     int idb = 0, flag = 0, esc = 0;
 
+    //inicio do loop princiapl
     do {
         flag = 0;
         printf("Digite o ID para excluir ou digite [0] para cancelar:\n ");
@@ -17,15 +16,18 @@ int excluir(int *tamanho) {
             return 0;
         }
 
+        // percorrer o array de cadastros
         for (int i = 0; i < *tamanho; i++) {
-            if (idb == cadastro[i].id) {
+            if (idb == cadastro[i].id) { // verificar se o ID corresponde a um cadastro
                 flag = 1;
+                // exibe as informacoes
                 printf("Dono do Pet: %s\n", cadastro[i].dono);
                 printf("Nome do Pet: %s\n", cadastro[i].nome);
                 printf("Pet: %s\n", cadastro[i].pet);
                 printf("Raca do Pet: %s\n", cadastro[i].raca);
                 printf("Senha do Usuario: %s\n", cadastro[i].senha);
                 printf("Saldo: %.2f\n", cadastro[i].saldo);
+                // confirmacao para excluir
                 printf("Tem certeza que deseja excluir esse usuario? (Acao nao reversivel.)\n[1]Sim\n[2]Nao\n->");
                 if (scanf("%d", &esc) != 1) {
                     printf("Entrada invalida. Operacao cancelada.\n");
@@ -33,7 +35,7 @@ int excluir(int *tamanho) {
                     return -1;
                 }
 
-                if (esc == 1) {
+                if (esc == 1) { // excluindo o cadastro
                     if (i == *tamanho-1) {
                         cadastro = (struct dados*) realloc(cadastro, (*tamanho-1) * sizeof(struct dados));
                     } else {
@@ -53,7 +55,7 @@ int excluir(int *tamanho) {
             }
         }
 
-        if (flag == 0) {
+        if (flag == 0) { // se o ID nao foi encontrado
             printf("Conta nao encontrada!\n");
             printf("Deseja digitar novamente o ID da conta?\n[1]Sim\n[2]Nao: ");
             if (scanf("%d", &esc) != 1) {
@@ -64,6 +66,17 @@ int excluir(int *tamanho) {
         }
 
     } while (esc == 1);
+
+    // Ordenacao do array 'cadastro' por meio de bubble sort
+    for (int i = 0; i < *tamanho; i++) { //variaveis de controle
+        for (int j = i; j < *tamanho; j++) {
+            if (cadastro[i].id > cadastro[j].id) { //se i for maior que j, ocorre a troca crescente
+                struct dados temp = cadastro[i]; // struct dados temp armazena temporariamente uma cadastro durante a troca
+                cadastro[i] = cadastro[j];
+                cadastro[j] = temp;
+            }
+        }
+    }
 
     return 0;
 }
